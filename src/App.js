6d7738ec64d5d86103c1ce0df97ad4e552f47a63
1ivari <60034643+1ivari@users.noreply.github.com>
2022-05-27 14:39:54 +0300

    routes and about page works
    done on laptop, see if runs on desktop

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import AboutPage from './components/pages/AboutPage'
 import PerfTable from './components/PerfTable'
-import aircraftBasicInfo from './data/aircraftBasicInfo';
+import aircraftBasicInfo from './data/aircraftBasicInfo'
 
 function App() {
-
-// You can put functions and stuff here
+  // TODO:
+  // Add routes. Välilehdille hyvä käyttää NavLink, tällä saa esim aktiivisen välilehden helposti highlightattua CSS tyyleillä
+  // routesissa myös useNavigate jos pitää ensin suorittaa joku funktio ja sen jälkeen osoittaa uudelle sivulle.ks. lec 29
   const [aircrafts, createAircraft] = useState(aircraftBasicInfo)
 
   return (
-    <Router>
-      <Navbar />
-      <div className="flex flex-row">
-        <div className="basis-1/3">
-        <PerfTable aircrafts={aircrafts} />
+    <>
+      <Router>
+        <Navbar />
+        <div className='flex flex-row'>
+          <div className='basis-1/3'>
+            <Routes>
+              <Route
+                exact
+                path='/'
+                element={<PerfTable aircrafts={aircrafts} />}
+              />
+            </Routes>
+          </div>
         </div>
-      </div>
-      
-      
-      <main>Content</main>
-    </Router>
-  
-  );
+        <Routes>
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,38 @@
-import {FaPlaneDeparture} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import { FaPlaneDeparture } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-function Navbar({title}) {
+function Navbar({ title }) {
   return (
-    <div className="navbar bg-neutral text-neutral-content">
-        <div className="container mx-auto">
-            <div className="flex-none px-2 mx-2">
-                <Link to='/' className="btn normal-case text-xl">
-                <FaPlaneDeparture />
-                {title}
-                </Link>
-            </div>
-            <div className="flex-1 px-2 mx-2">
-                <div className="flex justify-end">
-                    <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
-                        Home
-                    </Link>
-                    <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
-                        About
-                    </Link>
-                </div>
-            </div>
+    <div className='navbar bg-neutral text-neutral-content'>
+      <div className='container mx-auto'>
+        <div className='flex-none px-2 mx-2'>
+          <Link to='/' className='btn normal-case text-xl'>
+            <FaPlaneDeparture />
+            {title}
+          </Link>
         </div>
+        <div className='flex-1 px-2 mx-2'>
+          <div className='flex justify-end'>
+            <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
+              Home
+            </Link>
+            <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
+              About
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
 Navbar.defaultProps = {
-    title: 'flySafe',
-
+  title: 'flySafe',
 }
 
 Navbar.propTypes = {
-    title: PropTypes.string,
+  title: PropTypes.string,
 }
 
 export default Navbar
