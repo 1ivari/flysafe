5eca611ef3f7b71118c87c3202614d88bc3e36c1
1ivari <60034643+1ivari@users.noreply.github.com>
2022-06-09 21:27:00 +0300

    progress airfieldsearch

--- src/components/AirfieldSearch.jsx
+++ src/components/AirfieldSearch.jsx
@@ -3,56 +3,69 @@ import AppContext from '../context/AppContext.jsx'
 import icao from '../data/icao.json'
 
 function AirfieldSearch() {
-  const { route, setRoute } = useContext(AppContext)
-  const [ap, setAp] = useState()
+	const { route, setRoute } = useContext(AppContext)
+	const [ap, setAp] = useState()
+	const searchList = []
 
-  const handleSearch = (e) => {
-    e.preventDefault()
-    setRoute([...route, ap])
-    console.log(`route set to ${ap}`)
-    console.log(`route looks now like this ${route}`)
-  }
+	const handleSearch = (e) => {
+		e.preventDefault()
+		setRoute([...route, ap])
+		console.log(`route set to ${ap}`)
+		console.log(`route looks now like this ${route}`)
+	}
 
-  const handleChange = (e) => {
-    e.preventDefault()
-    const res = icao.filter((ap) =>
-      ap.ident.includes(e.target.value.toUpperCase())
-    )
-    setAp(res)
-  }
+	const handleChange = (e) => {
+		e.preventDefault()
+		const res = icao.filter((ap) =>
+			ap.ident.includes(e.target.value.toUpperCase())
+		)
+		searchList.push(res.slice(0, 5))
+		console.log(res.length)
+		console.log(searchList[0][0].ident)
+		setAp(res)
+	}
 
-  return (
-    <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
-      <div>
-        <form action=''>
-          <div className='form-control'>
-            <div className='relative'>
-              <input
-                type='text'
-                className='w-full pr-40 bg-gray-200 input input-lg text-black'
-                placeholder='Search'
-                onChange={handleChange}
-              />
-              <button
-                className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'
-                type='submit'
-                onClick={handleSearch}
-              >
-                Go
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-      <div>
-        <button className='btn btn-ghost btn-lg'>Clear</button>
-      </div>
-      moroo:
-      {route.map((ap) => {
-        return <div key={ap[0].ident}>{ap[0].name}</div>
-      })}
-    </div>
-  )
+	return (
+		<div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
+			<div>
+				<form action=''>
+					<div className='form-control'>
+						<div className='relative'>
+							<input
+								type='text'
+								className='w-full pr-30 bg-gray-200 input input-sm text-black'
+								placeholder='Search'
+								onChange={handleChange}
+							/>
+							<button
+								className='absolute top-0 right-0 rounded-l-none w-8 btn btn-sm'
+								type='submit'
+								onClick={handleSearch}>
+								Go
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+			<div>
+				<button className='ml-4 btn btn-ghost btn-sm'>Clear</button>
+			</div>
+
+			<div className=''>
+				<ul id='result' className='menu'>
+					{searchList.map((ap) => {
+						return <li key={ap[0][0].ident}>{ap[0][0].name}</li>
+					})}
+				</ul>
+			</div>
+
+			<ul id='result' className='menu'>
+				{route.map((ap) => {
+					return <li key={ap[0].ident}>{ap[0].name}</li>
+				})}
+			</ul>
+		</div>
+	)
 }
 
 export default AirfieldSearch
--- src/components/pages/RoutePage.jsx
+++ src/components/pages/RoutePage.jsx
@@ -7,7 +7,16 @@ function RoutePage() {
 		<>
 			<ProgressSteps activePage={2} />
 			<PreviousNextBtn previousPage='/basicdata' nextPage='/Weather' />
-			<AirfieldSearch />
+
+			<div className='flex flex-col justify-center py-12 px-6 lg:px-8'>
+				<div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+					<div className='bg-white py-8 px-6 shadow rounded-lg sm:px-10'>
+						<div className='mb-0 space-y-6'>
+							<AirfieldSearch />
+						</div>
+					</div>
+				</div>
+			</div>
 		</>
 	)
 }
