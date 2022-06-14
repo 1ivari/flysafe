81243cf51d6ab481385e8888aa89ab661d1574fb
1ivari <60034643+1ivari@users.noreply.github.com>
2022-06-14 22:10:22 +0300

    ofp table trials

--- src/components/pages/OperationalFlightPlanPage.jsx
+++ src/components/pages/OperationalFlightPlanPage.jsx
@@ -1,12 +1,55 @@
+import React from 'react'
 import PreviousNextBtn from '../PreviousNextBtn'
 import ProgressSteps from '../ProgressSteps'
 
 function OperationalFlightPlanPage() {
+  // const createRow = () => {
+  //   const rowArr = []
+  //   for (let i = 1; i < 24; i++) {
+  //     let row = React.createElement('div', {}, i)
+  //     rowArr.push(row)
+  //   }
+  //   console.log(rowArr)
+  //   console.log('moi')
+  //   ReactDOM.render(rowArr, document.getElementById('global'))
+  // }
+
+  // createRow()
+
   return (
     <>
       <ProgressSteps activePage={4} />
       <PreviousNextBtn previousPage='/weather' nextPage='/wnb' />
-      <div className='grid grid-cols-24'></div>
+      <div className='grid grid-cols-24 gap-2 p-10' id='global'>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+        <div className='bg-neutral'>1</div>
+
+        {/* <input type='number' className='p-2 w-10' /> */}
+      </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require('daisyui'), require('@tailwindcss/forms')],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        // Simple 24 column grid
+        24: 'repeat(24, minmax(0, 1fr))',
+
+        // Complex site-specific column configuration
+        footer: '200px minmax(900px, 1fr) 100px',
+      },
+    },
+  },
+  plugins: [require('daisyui'), require('@tailwindcss/forms')],
 }
