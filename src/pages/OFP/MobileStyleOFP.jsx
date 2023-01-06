4e5e1a83bda9652a68cbab0cea61e7521d3fdce6
1ivari <60034643+1ivari@users.noreply.github.com>
2023-01-06 12:15:32 +0200

    animated arrows started

--- src/pages/OFP/MobileStyleOFP.jsx
+++ src/pages/OFP/MobileStyleOFP.jsx
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion'
+
 function MobileStyleOFP(props) {
   const { ofp, handleChangeRecalculate, toggle, toggleLock } = props
 
@@ -20,7 +22,23 @@ function MobileStyleOFP(props) {
             <div className='collapse-title grid grid-cols-4 justify-items-center'>
               <div className='text-sm font-medium'>{row.description}</div>
               <div className='text-sm'>{row.timeInt.format('HH:mm')}</div>
-              <div className='text-sm'>{row.mh.toFixed(0)}°</div>
+              <div className='text-sm flex flex-row '>
+                {row.mh.toFixed(0)}°
+                <motion.div
+                  animate={{ rotate: Number(row.tas) }}
+                  className='origin-center bg-base-300'
+                >
+                  <svg
+                    className='h-5 ml-2'
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 384 512'
+                  >
+                    {/* <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --> */}
+                    <path d='M214.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 109.3V480c0 17.7 14.3 32 32 32s32-14.3 32-32V109.3l73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128z' />
+                  </svg>
+                </motion.div>
+              </div>
+
               <div className='text-sm'>{row.distInt.toFixed(0)} NM</div>
             </div>
             <div className='collapse-content'>
